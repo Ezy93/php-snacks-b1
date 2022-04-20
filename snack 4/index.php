fbe3faa509d8c9c8da91cbcf843799a7e4ea8755
Ezy93 <giordani001@gmail.com>
2022-04-20 18:57:10 +0200

    refactoring snack 4 eliminato var_dumb e aggiunto codice html per la visualizzazione degli int contenuti nell'array

--- snack 4/index.php
+++ snack 4/index.php
@@ -7,5 +7,24 @@
             array_push($randomIntArray,$randomInt);
         }
     }
-    var_dump($randomIntArray);
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php snack 4</title>
+</head>
+<body>
+    <ul>
+        <?php for ($i=0; $i < count($randomIntArray); $i++) {?>
+            <li>
+                <?php echo $randomIntArray[$i] ?>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
